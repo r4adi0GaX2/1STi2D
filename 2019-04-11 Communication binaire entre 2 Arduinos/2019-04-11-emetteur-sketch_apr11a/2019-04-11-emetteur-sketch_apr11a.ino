1577ae527cc720799b9c905e90334d4ca58fd744
r4adi0GaX2 <r4adi0GaX2@protonmail.com>
2019-05-09 15:04:37 +0200

    Create 2019-04-11-emetteur-sketch_apr11a.ino

---
+++ 2019-04-11 Communication binaire entre 2 Arduinos/2019-04-11-emetteur-sketch_apr11a/2019-04-11-emetteur-sketch_apr11a.ino
@@ -0,0 +1,32 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(9,OUTPUT);
+
+}
+
+void loop() {
+  int message, one_bit, counter;
+  if (Serial.available ()) {
+    message = Serial.parseInt ();
+          Serial.println(message);
+
+    digitalWrite (9, HIGH);
+    delay (310);
+    digitalWrite (9, LOW);
+    for (counter = 0; counter < 8; counter++) {
+      one_bit = message % 2;
+      delay (100);
+      digitalWrite (9, HIGH);
+      if (one_bit == 1) {
+        delay (210);
+        digitalWrite (9, LOW);
+      } else {
+        delay(110);
+        digitalWrite (9, LOW);
+        delay(100);
+      }
+      message = message / 2;
+    }
+  }
+
+}
