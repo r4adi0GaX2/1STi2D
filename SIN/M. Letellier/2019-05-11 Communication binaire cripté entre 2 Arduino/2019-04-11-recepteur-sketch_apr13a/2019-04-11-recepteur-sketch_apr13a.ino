299f8bb05cc36335afd4943a3ddd2695f6af0b19
r4adi0GaX2 <r4adi0GaX2@protonmail.com>
2019-05-12 01:43:55 +0200

    Update 2019-04-11-recepteur-sketch_apr13a.ino

--- SIN/M. Letellier/2019-05-11 Communication binaire cripté entre 2 Arduino/2019-04-11-recepteur-sketch_apr13a/2019-04-11-recepteur-sketch_apr13a.ino
+++ SIN/M. Letellier/2019-05-11 Communication binaire cripté entre 2 Arduino/2019-04-11-recepteur-sketch_apr13a/2019-04-11-recepteur-sketch_apr13a.ino
@@ -1,4 +1,6 @@
 #define BROCHE 10
+char password;
+int getpassword = 0;
 
 unsigned long duree_reception ;
 int one_bit, message, counter, poids;
@@ -9,16 +11,27 @@ void setup() {
 }
 
 void loop() {
+
+    while (getpassword == 0) {
+    Serial.println("Set a password");
+    delay(5000);
+    if (Serial.available()) {
+      Serial.println("Password set.");
+      password = Serial.read();
+      getpassword = !getpassword;
+    }
+  }
+  
   duree_reception = pulseIn (BROCHE, HIGH, 4000000);
 //  Serial.println(duree_reception);
 
-  if ((duree_reception / 100) == 3) {
+  if ((duree_reception / 100*password/1000) == 3) {
     message = 0 ;
     poids = 1 ;
     for (int i = 0 ; i < 8 ; i++) {
       duree_reception = pulseIn (BROCHE, HIGH, 4000000);
 //       Serial.println(duree_reception);
-      one_bit = (duree_reception / 100) - 1 ;
+      one_bit = (duree_reception / 100*password/1000) - 1 ;
       message = (message + one_bit * poids) ;
       poids = (poids * 2) ;
     }
