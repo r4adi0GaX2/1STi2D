cefb09c25fb1bc6e7fb3ec061d38ea25b97b2a54
r4adi0GaX2 <r4adi0GaX2@protonmail.com>
2019-05-11 20:01:46 +0200

    communication avec text

--- SIN/M. Letellier/2019-04-11 Communication binaire entre 2 Arduino/2019-04-11-emetteur-sketch_apr11a/2019-04-11-emetteur-sketch_apr11a.ino
+++ SIN/M. Letellier/2019-04-11 Communication binaire entre 2 Arduino/2019-04-11-emetteur-sketch_apr11a/2019-04-11-emetteur-sketch_apr11a.ino
@@ -5,27 +5,28 @@ void setup() {
 }
 
 void loop() {
-  int message, one_bit, counter;
+  int one_bit, counter;
+  int message;
   
   if (Serial.available ()) {
-    message = Serial.parseInt ();
-    Serial.println(message);
+    message = Serial.read ();
+    Serial.print(message);
     digitalWrite (9, HIGH);
-    delay (310);
+    delayMicroseconds (310);
     digitalWrite (9, LOW);
     
     for (counter = 0; counter < 8; counter++) {
       one_bit = message % 2;
-      delay (100);
+      delayMicroseconds (100);
       digitalWrite (9, HIGH);
       
       if (one_bit == 1) {
-        delay (210);
+        delayMicroseconds (210);
         digitalWrite (9, LOW);
       } else {
-        delay(110);
+        delayMicroseconds(110);
         digitalWrite (9, LOW);
-        delay(100);
+        delayMicroseconds(100);
       }
       message = message / 2;
     }
