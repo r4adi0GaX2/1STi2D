a4cb18bcad4798e3cbfcf6db426b07f1d77e70c7
r4adi0GaX2 <r4adi0GaX2@protonmail.com>
2019-05-11 20:02:07 +0200

    communication avec text

--- SIN/M. Letellier/2019-04-11 Communication binaire entre 2 Arduino/2019-04-11-recepteur-sketch_apr13a/2019-04-11-recepteur-sketch_apr13a.ino
+++ SIN/M. Letellier/2019-04-11 Communication binaire entre 2 Arduino/2019-04-11-recepteur-sketch_apr13a/2019-04-11-recepteur-sketch_apr13a.ino
@@ -12,16 +12,16 @@ void loop() {
   duree_reception = pulseIn (BROCHE, HIGH, 4000000);
 //  Serial.println(duree_reception);
 
-  if ((duree_reception / 100000) == 3) {
+  if ((duree_reception / 100) == 3) {
     message = 0 ;
     poids = 1 ;
     for (int i = 0 ; i < 8 ; i++) {
       duree_reception = pulseIn (BROCHE, HIGH, 4000000);
 //       Serial.println(duree_reception);
-      one_bit = (duree_reception / 100000) - 1 ;
+      one_bit = (duree_reception / 100) - 1 ;
       message = (message + one_bit * poids) ;
       poids = (poids * 2) ;
     }
-  Serial.println(message);  
+  Serial.print(char(message));  
   }
 }
