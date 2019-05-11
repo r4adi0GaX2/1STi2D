e45e442d6c01945847cdf67a7bdd628d8c31ac7c
r4adi0GaX2 <r4adi0GaX2@protonmail.com>
2019-05-11 20:12:23 +0200

    binaire cripté

---
+++ SIN/M. Letellier/2019-05-11 Communication binaire cripté entre 2 Arduino/2019-04-11-emetteur-sketch_apr11a/2019-04-11-emetteur-sketch_apr11a.ino
@@ -0,0 +1,35 @@
+void setup() {
+  Serial.begin(9600);
+  pinMode(9,OUTPUT);
+
+}
+
+void loop() {
+  int one_bit, counter;
+  int message;
+  
+  if (Serial.available ()) {
+    message = Serial.read ();
+    Serial.print(message);
+    digitalWrite (9, HIGH);
+    delayMicroseconds (310);
+    digitalWrite (9, LOW);
+    
+    for (counter = 0; counter < 8; counter++) {
+      one_bit = message % 2;
+      delayMicroseconds (100);
+      digitalWrite (9, HIGH);
+      
+      if (one_bit == 1) {
+        delayMicroseconds (210);
+        digitalWrite (9, LOW);
+      } else {
+        delayMicroseconds(110);
+        digitalWrite (9, LOW);
+        delayMicroseconds(100);
+      }
+      message = message / 2;
+    }
+  }
+
+}
---
+++ SIN/M. Letellier/2019-05-11 Communication binaire cripté entre 2 Arduino/2019-04-11-recepteur-sketch_apr13a/2019-04-11-recepteur-sketch_apr13a.ino
@@ -0,0 +1,27 @@
+#define BROCHE 10
+
+unsigned long duree_reception ;
+int one_bit, message, counter, poids;
+
+void setup() {
+  pinMode(BROCHE, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  duree_reception = pulseIn (BROCHE, HIGH, 4000000);
+//  Serial.println(duree_reception);
+
+  if ((duree_reception / 100) == 3) {
+    message = 0 ;
+    poids = 1 ;
+    for (int i = 0 ; i < 8 ; i++) {
+      duree_reception = pulseIn (BROCHE, HIGH, 4000000);
+//       Serial.println(duree_reception);
+      one_bit = (duree_reception / 100) - 1 ;
+      message = (message + one_bit * poids) ;
+      poids = (poids * 2) ;
+    }
+  Serial.print(char(message));  
+  }
+}
