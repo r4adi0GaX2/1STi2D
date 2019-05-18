0189c4996e86ad036facad89c11d6d778c97d8aa
r4adi0GaX2 <r4adi0GaX2@protonmail.com>
2019-05-18 15:39:40 +0200

    serveur_avec_capteur_tmprtr_et_hmdt

--- SIN/M. Tissier/2019-05-16 Capteur de températeur et d'humidité/Server_ESP8266_pour_capteur/Server_ESP8266_pour_capteur.ino
+++ SIN/M. Tissier/2019-05-16 Capteur de températeur et d'humidité/Server_ESP8266_pour_capteur/Server_ESP8266_pour_capteur.ino
@@ -3,8 +3,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char* ssid = "NodeMCU";
-const char* password = "NodeMCU";
+const char* ssid = "...";
+const char* password = "...";
 
 ESP8266WebServer server(80);
 
